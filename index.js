7f011e79ae566f11d5a2b7991e3a17e98bb48f48
markglasman <glasman.mark@gmail.com>
2024-08-16 00:16:38 -0400

    completed game functionality via checkWinner()

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function cellClicked() {
   }
 
   updateCell(this, cellIndex);
-  changePlayer();
+//   changePlayer();
   checkWinner();
 }
 
@@ -42,9 +42,36 @@ function updateCell(cell, index) {
 }
 
 function changePlayer() {
-  currentPlayer = currentPlayer == "X" ? "O" : "X";
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
   statusText.textContent = `${currentPlayer}'s turn`;
 }
-function checkWinner() {}
+function checkWinner() {
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == "") {
+            continue
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`
+
+        running = false
+    } else if(!options.includes("")){
+        statusText.textContent = 'Draw!';
+        running = false;
+    } else{
+        changePlayer();
+    }
+}
 
 function restartGame() {}
