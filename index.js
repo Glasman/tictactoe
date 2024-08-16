366885ba81f10912fc8703915667cac8cbc82af4
markglasman <glasman.mark@gmail.com>
2024-08-15 23:40:21 -0400

    fixed formatting

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const cells = document.querySelectorAll(".cell")
-const statusText = document.querySelector("#statusText")
-const restartBtn = document.querySelector("#restartBtn")
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
 const winConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
@@ -17,38 +17,34 @@ let running = false;
 
 initializeGame();
 
-function initializeGame(){
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));
-    restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
-    running = true;
+function initializeGame() {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  restartBtn.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+  running = true;
 }
 
-function cellClicked(){
-    const cellIndex = this.getAttribute("cellIndex")
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
 
-    if(options[cellIndex] != "" || !running) {
-        return;
-    }
+  if (options[cellIndex] != "" || !running) {
+    return;
+  }
 
-    updateCell(this, cellIndex);
-    changePlayer();
-    checkWinner();
+  updateCell(this, cellIndex);
+  changePlayer();
+  checkWinner();
 }
 
-function updateCell(cell, index){
-    options[index] = currentPlayer;
-    cell.textContent = currentPlayer;
+function updateCell(cell, index) {
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer;
 }
 
-function changePlayer(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    statusText.textContent = `${currentPlayer}'s turn`
+function changePlayer() {
+  currentPlayer = currentPlayer == "X" ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
 }
-function checkWinner() {
+function checkWinner() {}
 
-}
-
-function restartGame(){
-    
-}
+function restartGame() {}
